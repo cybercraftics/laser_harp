8b71d83c9b960917c68b76e758009dcaeaa2a3a2
Harry Mishinev <harry@mishinev.com>
2024-11-22 14:56:49 +0200

    Initial version of the harp

---
+++ laser_harp_with_servo_motor/laser_harp_with_servo_motor.ino
@@ -0,0 +1,40 @@
+const int dirPin = 2;  // Direction pin
+const int stepPin = 3; // Step pin
+
+// Define motor and harp properties
+const int TOTAL_STEPS = 20;         // Total travel distance in steps
+const int NUM_STRINGS = 6;          // Number of laser harp strings
+const int STEPS_PER_STRING = TOTAL_STEPS / NUM_STRINGS; // Steps between strings
+const int STEP_DELAY = 600;         // Minimum reliable delay between steps in microseconds
+const int PAUSE_DELAY = 10;         // Pause at each string in milliseconds
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
+  // Set initial direction
+  digitalWrite(dirPin, HIGH);
+}
+
+void loop() {
+  // Travel through all string positions
+  for (int i = 0; i < NUM_STRINGS; i++) {
+    // Move the motor to the next string
+    moveSteps(STEPS_PER_STRING);
+    // Briefly pause to "draw" the string
+    delay(PAUSE_DELAY);
+  }
+
+  // Reverse direction for the return path
+  digitalWrite(dirPin, !digitalRead(dirPin));
+}
+
+// Function to move a specific number of steps at maximum speed
+void moveSteps(int steps) {
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(STEP_DELAY); // Short delay for maximum speed
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(STEP_DELAY);
+  }
+}
