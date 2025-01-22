7d918dde0fb3df1dfe804fe787c5ddb3d7833f1f
Harry Mishinev <harry@mishinev.com>
2025-01-22 14:52:45 +0200

    Change the code to controll an RGB Laser

--- stepper_test_ULN2003_driver/stepper_test_ULN2003_driver.ino
+++ stepper_test_ULN2003_driver/stepper_test_ULN2003_driver.ino
@@ -1,19 +1,46 @@
 #include <Stepper.h>
 
-const float STEPS_PER_REVOLUTION = 50;
+// #define LASER_PIN 12
+#define LASER_PIN_1 2
+#define LASER_PIN_2 3
+#define LASER_PIN_3 4
+
+const float STEPS_PER_REVOLUTION = 1000;
+const unsigned long LASER_ON_DURATION = 1; // Laser on for 1 ms
 
 Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
 
 void setup() {
-  stepper.setSpeed(1000);
+  stepper.setSpeed(50);
+  pinMode(LASER_PIN_1, OUTPUT);
+  pinMode(LASER_PIN_2, OUTPUT);
+  pinMode(LASER_PIN_3, OUTPUT);
 }
 
 void loop() {
-    stepper.step(18);
-    delay(1);
-    stepper.step(-18);
-    delay(1);
+  // Forward 7 times
+  for (int i = 0; i < 4; i++) {
+    stepper.step(2);
+    delay(10);
+    digitalWrite(LASER_PIN_1, HIGH);
+    digitalWrite(LASER_PIN_2, HIGH);
+    digitalWrite(LASER_PIN_3, HIGH);
+    delay(3);
+    digitalWrite(LASER_PIN_1, LOW);
+    digitalWrite(LASER_PIN_2, LOW);
+    digitalWrite(LASER_PIN_3, LOW);
+  }
+
+  // Backward 7 times
+  for (int i = 0; i < 4; i++) {
+    stepper.step(-2);
+    delay(10);
+    digitalWrite(LASER_PIN_1, HIGH);
+    digitalWrite(LASER_PIN_2, HIGH);
+    digitalWrite(LASER_PIN_3, HIGH);
+    delay(3);
+    digitalWrite(LASER_PIN_1, LOW);
+    digitalWrite(LASER_PIN_2, LOW);
+    digitalWrite(LASER_PIN_3, LOW);
+  }
 }
-
-
-
