4ba13c6561e4b4c315925d1447c02802b5099e37
Harry Mishinev <harry@mishinev.com>
2025-01-23 18:49:03 +0200

    Best motor settings

--- stepper_test_ULN2003_driver/stepper_test_ULN2003_driver.ino
+++ stepper_test_ULN2003_driver/stepper_test_ULN2003_driver.ino
@@ -3,13 +3,13 @@
 // #define LASER_PIN 12
 #define LASER_PIN_1 2
 
-const float STEPS_PER_REVOLUTION = 1000;
+const float STEPS_PER_REVOLUTION = 200;
 const unsigned long LASER_ON_DURATION = 1; // Laser on for 1 ms
 
 Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
 
 void setup() {
-  stepper.setSpeed(40);
+  stepper.setSpeed(220);
   pinMode(LASER_PIN_1, OUTPUT);
 }
 
@@ -17,7 +17,11 @@ void loop() {
   // Forward 7 times
   for (int i = 0; i < 5; i++) {
       stepper.step(2);
-      delay(4);
+      if (i == 0) {
+        delay(5);
+      } else {
+        delay(4);
+      }
       digitalWrite(LASER_PIN_1, HIGH);
       delay(1);
       digitalWrite(LASER_PIN_1, LOW);
