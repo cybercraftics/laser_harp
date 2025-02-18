7834bcb0b0d6a82459db32b11a8f6dc811f4c768
Harry Mishinev <harry@mishinev.com>
2025-02-18 16:24:37 +0200

    Random improvements

--- main_single_sonic_sensor_loop_slow/main_single_sonic_sensor_loop_slow.ino
+++ main_single_sonic_sensor_loop_slow/main_single_sonic_sensor_loop_slow.ino
@@ -1,31 +1,26 @@
 #include <Stepper.h>
 
-// Pin definitions
-#define LASER_PIN 4
-#define LIGHT_DETECTOR_PIN_1 A0
-#define LIGHT_DETECTOR_PIN_2 A1
-#define LIGHT_DETECTOR_PIN_3 A2
-#define LIGHT_DETECTOR_PIN_4 A3
-#define LIGHT_DETECTOR_PIN_5 A4
-#define CLOSED_LOOP_MOTOR_SENSOR_PIN A5
+#define LIGHT_DETECTOR_PIN_1             A0
+#define LIGHT_DETECTOR_PIN_2             A1
+#define LIGHT_DETECTOR_PIN_3             A2
+#define LIGHT_DETECTOR_PIN_4             A3
+#define LIGHT_DETECTOR_PIN_5             A4
+#define CLOSED_LOOP_MOTOR_SENSOR_PIN     A5
 
-// Ultrasonic pins
-#define ULTRASONIC_SENSOR_1_TRIGGER_PIN 7
-#define ULTRASONIC_SENSOR_1_ECHO_PIN 2
-#define ULTRASONIC_SENSOR_2_TRIGGER_PIN 3
-#define ULTRASONIC_SENSOR_2_ECHO_PIN 5
+#define ULTRASONIC_SENSOR_1_TRIGGER_PIN   2
+#define ULTRASONIC_SENSOR_1_ECHO_PIN      3
+#define ULTRASONIC_SENSOR_2_TRIGGER_PIN   4
+#define ULTRASONIC_SENSOR_2_ECHO_PIN      5
 
-// Buzzer pin
-#define BUZZER_PIN 6
+#define BUZZER_PIN                        6
+#define LASER_PIN                         7
 
-// Stepper config
-const float STEPS_PER_REVOLUTION = 200;
-const unsigned int LASER_ON_DURATION = 150;
-const unsigned int NUMBER_OF_STRINGS = 7;
+const float        STEPS_PER_REVOLUTION = 200;
+const unsigned int LASER_ON_DURATION    = 150;
+const unsigned int NUMBER_OF_STRINGS    = 7;
 
 Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
 
-// Use a smaller median window for faster response
 const int NUM_SAMPLES = 3;
 
 // Higher smoothing factor -> more weight on new readings (faster response)
@@ -35,6 +30,7 @@ const float SMOOTHING_FACTOR = 0.4f;  // 0.4 is more responsive than 0.2
 
 void setup() {
     Serial.begin(115200);
+    Serial1.begin(9600);
     stepper.setSpeed(220);
 
     pinMode(LASER_PIN, OUTPUT);
@@ -51,50 +47,49 @@ void setup() {
     pinMode(ULTRASONIC_SENSOR_1_ECHO_PIN, INPUT);
     pinMode(ULTRASONIC_SENSOR_2_TRIGGER_PIN, OUTPUT);
     pinMode(ULTRASONIC_SENSOR_2_ECHO_PIN, INPUT);
-
-    Serial.println("Ultrasonic + Stepper + Buzzer (More Responsive) started...");
 }
 
 void loop() {
-    // Move stepper and blink laser (as in your original code)
-    // for (int i = 0; i < NUMBER_OF_STRINGS; i++) {
-    //     stepper.step(2);
+    for (int i = 0; i < NUMBER_OF_STRINGS; i++) {
+        stepper.step(2);
         
-    //     if (i == 3) {
-    //         delayMicroseconds(1000);
-    //     }
-    //     if (i == 4 || i == 5 || i == 6) {
-    //         delayMicroseconds(800);
-    //     }
+        if (i == 3) {
+            delayMicroseconds(1000);
+        }
+        if (i == 4 || i == 5 || i == 6) {
+            delayMicroseconds(800);
+        }
 
-    //     analogWrite(LASER_PIN, 255);
-    //     delayMicroseconds(LASER_ON_DURATION);
-    //     analogWrite(LASER_PIN, 0);
-    // }
-    // stepper.step(-NUMBER_OF_STRINGS * 2);
+        analogWrite(LASER_PIN, 255);
+        delayMicroseconds(LASER_ON_DURATION);
+        analogWrite(LASER_PIN, 0);
+    }
+    stepper.step(-NUMBER_OF_STRINGS * 2);
 
-    // 1) Get a median-filtered reading (just 3 samples now)
-    float medianDistance1 = getMedianDistance(ULTRASONIC_SENSOR_1_TRIGGER_PIN, ULTRASONIC_SENSOR_1_ECHO_PIN);
-    float medianDistance2 = getMedianDistance(ULTRASONIC_SENSOR_2_TRIGGER_PIN, ULTRASONIC_SENSOR_2_ECHO_PIN);
+    float distance1 = getRawDistance(ULTRASONIC_SENSOR_1_TRIGGER_PIN, ULTRASONIC_SENSOR_1_ECHO_PIN);
 
-    // 2) Exponential smoothing with a higher SMOOTHING_FACTOR
-    filteredDistance1 = updateFilteredDistance(filteredDistance1, medianDistance1);
-    filteredDistance2 = updateFilteredDistance(filteredDistance2, medianDistance2);
+    Serial.print("Distance 1: ");
+    Serial.println(distance1);
 
-    // Play separate sounds for each sensor
-    // if (filteredDistance1 > 0) {
-    //     playHighPitch(filteredDistance1);
+    // // 1) Get a median-filtered reading (just 3 samples now)
+    // float medianDistance1 = getMedianDistance(ULTRASONIC_SENSOR_1_TRIGGER_PIN, ULTRASONIC_SENSOR_1_ECHO_PIN);
+    // float medianDistance2 = getMedianDistance(ULTRASONIC_SENSOR_2_TRIGGER_PIN, ULTRASONIC_SENSOR_2_ECHO_PIN);
+
+    // // 2) Exponential smoothing with a higher SMOOTHING_FACTOR
+    // filteredDistance1 = updateFilteredDistance(filteredDistance1, medianDistance1);
+    // filteredDistance2 = updateFilteredDistance(filteredDistance2, medianDistance2);
+
+    // // Play separate sounds for each sensor
+    // // if (filteredDistance1 > 0) {
+    // //     playHighPitch(filteredDistance1);
+    // // } else {
+    // //     noTone(BUZZER_PIN);
+    // // }
+    // if (filteredDistance2 > 0) {
+    //     playLowPitch(filteredDistance2);
     // } else {
     //     noTone(BUZZER_PIN);
     // }
-    if (filteredDistance2 > 0) {
-        playLowPitch(filteredDistance2);
-    } else {
-        noTone(BUZZER_PIN);
-    }
-
-    // Reduce the overall loop delay for faster updates
-    delay(20);
 }
 
 // (1) MEDIAN FILTERING of 3 samples -> faster sampling and less overkill smoothing
@@ -133,13 +128,9 @@ float getRawDistance(int triggerPin, int echoPin) {
     digitalWrite(triggerPin, LOW);
 
     // 45ms for ~4.5m
-    long duration = pulseIn(echoPin, HIGH, 40000);
+    long duration = pulseIn(echoPin, HIGH, 45000);
 
-    float distance = duration * 0.0343f / 2.0f; 
-    if (distance > 2 && distance <= 150) {
-        return distance;
-    }
-    return -1;
+    return duration * 0.0343f / 2.0f;
 }
 
 // (2) Exponential smoothing
